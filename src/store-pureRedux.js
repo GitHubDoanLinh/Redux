d4b6f6b8304d2f1d132248c57f82c5cf78936811
Doan Huu Linh <doanlinh1510@gmail.com>
2024-11-20 04:04:54 +0700

    Creating the Store With Redux toolkit

--- src/store-pureRedux.js
+++ src/store-pureRedux.js
@@ -1,8 +1,9 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
-import { thunk } from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { thunk } from "redux-thunk";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
+
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
